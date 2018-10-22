7950d0402f7f88e01a25508bcc315b8abf444417
Aykut Yilmaz <aykuxt@gmail.com>
2018-10-22 22:06:49 +0200

    www: new homepage
    - brand new homepage
    - material-kit updated to v2.0.4
    - added original scss files (they were now automatically converted by
      jekyll)
    - bug fixes
    - blog moved to lineageos.org/blog
    
    Change-Id: I21c438df6c5e2e45da7f5e80163e6e6a54f73948

--- assets/js/material-kit.js
+++ assets/js/material-kit.js
@@ -65,44 +65,44 @@ $(document).ready(function() {
 });
 
 $(document).on('click', '.navbar-toggler', function() {
-    $toggle = $(this);
+  $toggle = $(this);
 
-    if (materialKit.misc.navbar_menu_visible == 1) {
-        $('html').removeClass('nav-open');
-        materialKit.misc.navbar_menu_visible = 0;
-        $('#bodyClick').remove();
-        setTimeout(function() {
-            $toggle.removeClass('toggled');
-        }, 550);
+  if (materialKit.misc.navbar_menu_visible == 1) {
+    $('html').removeClass('nav-open');
+    materialKit.misc.navbar_menu_visible = 0;
+    $('#bodyClick').remove();
+    setTimeout(function() {
+      $toggle.removeClass('toggled');
+    }, 550);
 
+    $('html').removeClass('nav-open-absolute');
+  } else {
+    setTimeout(function() {
+      $toggle.addClass('toggled');
+    }, 580);
+
+
+    div = '<div id="bodyClick"></div>';
+    $(div).appendTo("body").click(function() {
+      $('html').removeClass('nav-open');
+
+      if ($('nav').hasClass('navbar-absolute')) {
         $('html').removeClass('nav-open-absolute');
-    } else {
-        setTimeout(function() {
-            $toggle.addClass('toggled');
-        }, 580);
+      }
+      materialKit.misc.navbar_menu_visible = 0;
+      $('#bodyClick').remove();
+      setTimeout(function() {
+        $toggle.removeClass('toggled');
+      }, 550);
+    });
 
-
-        div = '<div id="bodyClick"></div>';
-        $(div).appendTo("body").click(function() {
-            $('html').removeClass('nav-open');
-
-            if ($('nav').hasClass('navbar-absolute')) {
-                $('html').removeClass('nav-open-absolute');
-            }
-            materialKit.misc.navbar_menu_visible = 0;
-            $('#bodyClick').remove();
-            setTimeout(function() {
-                $toggle.removeClass('toggled');
-            }, 550);
-        });
-
-        if ($('nav').hasClass('navbar-absolute')) {
-            $('html').addClass('nav-open-absolute');
-        }
-
-        $('html').addClass('nav-open');
-        materialKit.misc.navbar_menu_visible = 1;
+    if ($('nav').hasClass('navbar-absolute')) {
+      $('html').addClass('nav-open-absolute');
     }
+
+    $('html').addClass('nav-open');
+    materialKit.misc.navbar_menu_visible = 1;
+  }
 });
 
 materialKit = {
