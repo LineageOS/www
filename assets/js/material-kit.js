3f031e966f2efa0794e87a14d510a3d2f1e225ab
Aykut Yilmaz <aykuxt@gmail.com>
2018-10-22 22:06:49 +0200

    www: new homepage
    - brand new homepage
    - material-kit updated to v2.0.4
    - added original scss files (they were now automatically converted by
      jekyll)
    - bug fixes
    - blog moved to lineageos.org/blog
    
    Change-Id: I21c438df6c5e2e45da7f5e80163e6e6a54f73948
    Signed-off-by: Joey <joey@lineageos.org>

--- assets/js/material-kit.js
+++ assets/js/material-kit.js
@@ -41,23 +41,19 @@ $(document.links).filter(function() {
   return this.hostname != window.location.hostname;
 }).attr('target', '_blank');
 
-/*! =========================================================
- *
- * Material Kit Free - v2.0.2 (Bootstrap 4.0.0 Final Edition)
- *
- * =========================================================
- *
- * Product Page: https://www.creative-tim.com/product/material-kit-pro
- * Available with purchase of license from http://www.creative-tim.com/product/material-kit-pro
- * Copyright 2017 Creative Tim (https://www.creative-tim.com)
- * License Creative Tim (https://www.creative-tim.com/license)
- *
- * ========================================================= */
 
+/*!
+=========================================================
+* Material Kit - v2.0.4
+=========================================================
+* Product Page: https://www.creative-tim.com/product/material-kit
+* Copyright 2018 Creative Tim (http://www.creative-tim.com)
+=========================================================
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
 
 $(document).ready(function() {
 
-    // Init Material scripts for buttons ripples, inputs animations etc, more info on the next link https://github.com/FezVrasta/bootstrap-material-design#materialjs
     $('body').bootstrapMaterialDesign();
     window_width = $(window).width();
     $navbar_collapse = $('.navbar').find('.navbar-collapse');
@@ -65,44 +61,32 @@ $(document).ready(function() {
 });
 
 $(document).on('click', '.navbar-toggler', function() {
-    $toggle = $(this);
+  $toggle = $(this);
 
-    if (materialKit.misc.navbar_menu_visible == 1) {
-        $('html').removeClass('nav-open');
-        materialKit.misc.navbar_menu_visible = 0;
-        $('#bodyClick').remove();
-        setTimeout(function() {
-            $toggle.removeClass('toggled');
-        }, 550);
+  if (materialKit.misc.navbar_menu_visible == 1) {
+    $('html').removeClass('nav-open');
+    materialKit.misc.navbar_menu_visible = 0;
+    $('#bodyClick').remove();
+    setTimeout(function() {
+      $toggle.removeClass('toggled');
+    }, 550);
+  } else {
+    setTimeout(function() {
+      $toggle.addClass('toggled');
+    }, 580);
+    div = '<div id="bodyClick"></div>';
+    $(div).appendTo("body").click(function() {
+      $('html').removeClass('nav-open');
+      materialKit.misc.navbar_menu_visible = 0;
+      $('#bodyClick').remove();
+      setTimeout(function() {
+        $toggle.removeClass('toggled');
+      }, 550);
+    });
 
-        $('html').removeClass('nav-open-absolute');
-    } else {
-        setTimeout(function() {
-            $toggle.addClass('toggled');
-        }, 580);
-
-
-        div = '<div id="bodyClick"></div>';
-        $(div).appendTo("body").click(function() {
-            $('html').removeClass('nav-open');
-
-            if ($('nav').hasClass('navbar-absolute')) {
-                $('html').removeClass('nav-open-absolute');
-            }
-            materialKit.misc.navbar_menu_visible = 0;
-            $('#bodyClick').remove();
-            setTimeout(function() {
-                $toggle.removeClass('toggled');
-            }, 550);
-        });
-
-        if ($('nav').hasClass('navbar-absolute')) {
-            $('html').addClass('nav-open-absolute');
-        }
-
-        $('html').addClass('nav-open');
-        materialKit.misc.navbar_menu_visible = 1;
-    }
+    $('html').addClass('nav-open');
+    materialKit.misc.navbar_menu_visible = 1;
+  }
 });
 
 materialKit = {
@@ -115,19 +99,4 @@ materialKit = {
     navbar_initialized: false,
     isWindow: document.documentMode || /Edge/.test(navigator.userAgent)
   },
-
 };
-
-function debounce(func, wait, immediate) {
-    var timeout;
-    return function() {
-        var context = this,
-            args = arguments;
-        clearTimeout(timeout);
-        timeout = setTimeout(function() {
-            timeout = null;
-            if (!immediate) func.apply(context, args);
-        }, wait);
-        if (immediate && !timeout) func.apply(context, args);
-    };
-}
